7bfd617c52947b723af43917d1308b0cbf7f89a7
William McQuaid <whmcquaid@gmail.com>
2025-10-22 18:55:32 -0400

    Add darwin onboarding script.

---
+++ darwin-onboarding.sh
@@ -0,0 +1,27 @@
+# homebrew should be installed
+if (brew --version)
+then
+  echo "homebrew is already installed."
+else
+  echo "installing homebrew..."
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# nano should be installed
+if (which nano)
+then
+  echo "nano is already installed."
+else
+  echo "installing nano..."
+  brew install nano
+fi
+
+# git should be installed
+if (git --version)
+then
+  echo "git is already installed."
+else
+  echo "installing git..."
+  brew install git
+fi
+
